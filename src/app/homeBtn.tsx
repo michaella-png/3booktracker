b7b7f0848057858b4e8982a8d34f9a3f524bd072
Michaella Napier <2421400515@k12.hi.us>
2025-04-22 08:47:51 -1000

    routing buttons again fr

--- src/app/homeBtn.tsx
+++ src/app/homeBtn.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const HomePage: React.FC = () => {
     return (
-        <Link href="/app">
+        <Link href="/">
             <button className="padded-btn">Home page</button>
         </Link>
     );
